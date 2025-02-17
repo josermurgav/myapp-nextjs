8533869731a1b4859af5a2f93d0321c7a78afbc9
Jose Murga <josermurgav@gmail.com>
2025-02-16 22:44:03 -0300

    update img

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
 import Image from "next/image";
+import Script from "next/script";
 export default function Contact()
 {
     return( 
     <><h1>Contact Page</h1> 
         <Link href="/about">about</Link>
-        <Image src="/img/download.jpg" width={400} height={300} alt="Just a Chill guy"></Image>
+        <Image src="/img/download.jpg" 
+            width={400} 
+            height={300} 
+            alt="Just a Chill guy"
+            sizes="(max-width: 384 px) 100vw, 50vw"            
+            style={{objectFit:"cover"}}
+            ></Image>
     </>
     
 );
